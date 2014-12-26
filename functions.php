75c0f391e546485dac691c37415cfb369cd5ae5c
freepad <freepad@ya.ru>
2014-12-26 05:05:19 +0300

    add custom style for plugin BuddyPress

--- functions.php
+++ functions.php
@@ -6,6 +6,17 @@
  */
 	
 define( 'FORUM_VERSION', '1.0.0');
-define( 'FORUM_URI', get_template_directory_uri() );
+define( 'FORUM_URI', get_stylesheet_directory_uri() );
 define( 'FORUM_DIR', get_template_directory() );
 
+/**
+ * Enqueue scripts and styles
+ */
+add_action( 'wp_enqueue_scripts', 'forum_scripts' );
+function forum_scripts() {
+	// Forum stylesheets
+	wp_enqueue_style( 'forum-buddypress-custom-style', FORUM_URI . '/css/buddypress-custom.css', false, FORUM_VERSION );
+
+
+	// Forum scripts
+}
