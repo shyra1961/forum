c490605e20ea12534a14c03a028e411e21a52789
freepad <freepad@ya.ru>
2015-01-06 23:42:41 +0300

    fix загрузка стилей и скриптов зависимых от родительской темы

--- functions.php
+++ functions.php
@@ -10,16 +10,27 @@ define( 'FORUM_URI', get_stylesheet_directory_uri() );
 define( 'FORUM_DIR', get_template_directory() );
 
 /**
- * Enqueue scripts and styles
+ * Подключаем стили после того, как загрузится родительская тема,
+ * иначе подгрузка стилей/скриптов работающих в зависимости от 
+ * родительской темы не работает.
  */
-add_action( 'wp_enqueue_scripts', 'forum_scripts' );
-function forum_scripts() {
-	// Forum stylesheets
-	wp_enqueue_style( 'forum-buddypress-custom-style', FORUM_URI . '/assets/css/buddypress-custom.css', false, FORUM_VERSION );
+add_action ('init', 'child_theme_enqueue_scripts' );
+function child_theme_enqueue_scripts () {
+
+	/**
+	 * Enqueue scripts and styles
+	 */
+	add_action( 'wp_enqueue_scripts', 'forum_scripts' );
+	function forum_scripts() {
+		// Forum stylesheets
+		wp_enqueue_style( 'forum-buddypress-custom-style', FORUM_URI . '/assets/css/buddypress-custom.css', 'generate-child-css', FORUM_VERSION );
+		wp_enqueue_style( 'forum-style', FORUM_URI . '/assets/css/style.css', 'generate-child-css', FORUM_VERSION );
 
 
-	// Forum scripts
+		// Forum scripts
+	}
 }
+
 
 /**
  * Set default options
